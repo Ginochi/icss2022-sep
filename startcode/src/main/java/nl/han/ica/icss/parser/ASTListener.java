e627ca55040a9bc94857bfefcfff5ea4ef1d8a63
Gino Janssen <Ginojanssen23@gmail.com>
2024-10-08 12:01:19 +0200

    ASTListener code from lesson

--- startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
+++ startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
@@ -1,16 +1,9 @@
 package nl.han.ica.icss.parser;
 
-import java.util.Stack;
 
-
+import nl.han.ica.datastructures.HANStack;
 import nl.han.ica.datastructures.IHANStack;
 import nl.han.ica.icss.ast.*;
-import nl.han.ica.icss.ast.literals.*;
-import nl.han.ica.icss.ast.operations.AddOperation;
-import nl.han.ica.icss.ast.operations.MultiplyOperation;
-import nl.han.ica.icss.ast.operations.SubtractOperation;
-import nl.han.ica.icss.ast.selectors.ClassSelector;
-import nl.han.ica.icss.ast.selectors.IdSelector;
 import nl.han.ica.icss.ast.selectors.TagSelector;
 
 /**
@@ -26,10 +19,44 @@ public class ASTListener extends ICSSBaseListener {
 
 	public ASTListener() {
 		ast = new AST();
-		//currentContainer = new HANStack<>();
+		currentContainer = new HANStack<>();
 	}
     public AST getAST() {
         return ast;
     }
-    
+
+	@Override
+	public void enterStylesheet(ICSSParser.StylesheetContext ctx) {
+		Stylesheet stylesheet = new Stylesheet();
+		currentContainer.push(stylesheet);
+	}
+
+	@Override
+	public void exitStylesheet(ICSSParser.StylesheetContext ctx) {
+        ast.root = (Stylesheet) currentContainer.pop();
+	}
+
+	@Override
+	public void enterStylerule(ICSSParser.StyleruleContext ctx) {
+		Stylerule stylerule = new Stylerule();
+		currentContainer.push(stylerule);
+	}
+
+	@Override
+	public void exitStylerule(ICSSParser.StyleruleContext ctx) {
+		Stylerule rule = (Stylerule) currentContainer.pop();
+		currentContainer.peek().addChild(rule);
+	}
+
+	@Override
+	public void enterTagSelector(ICSSParser.TagSelectorContext ctx) {
+		TagSelector tagSelector = new TagSelector("temp TAG");
+		currentContainer.push(tagSelector);
+	}
+
+	@Override
+	public void exitTagSelector(ICSSParser.TagSelectorContext ctx) {
+		TagSelector selector = (TagSelector) currentContainer.pop();
+		currentContainer.peek().addChild(selector);
+	}
 }
