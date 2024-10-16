d9ea2bac068e42fab42c831155787f533a73580a
Gino Janssen <Ginojanssen23@gmail.com>
2024-10-16 09:55:50 +0200

    evaluator class demo code

--- startcode/src/main/java/nl/han/ica/icss/transforms/Evaluator.java
+++ startcode/src/main/java/nl/han/ica/icss/transforms/Evaluator.java
@@ -2,15 +2,8 @@ package nl.han.ica.icss.transforms;
 
 import nl.han.ica.datastructures.IHANLinkedList;
 import nl.han.ica.icss.ast.*;
-import nl.han.ica.icss.ast.literals.PercentageLiteral;
-import nl.han.ica.icss.ast.literals.PixelLiteral;
-import nl.han.ica.icss.ast.literals.ScalarLiteral;
-import nl.han.ica.icss.ast.operations.AddOperation;
-import nl.han.ica.icss.ast.operations.MultiplyOperation;
-import nl.han.ica.icss.ast.operations.SubtractOperation;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 
 public class Evaluator implements Transform {
 
@@ -23,8 +16,26 @@ public class Evaluator implements Transform {
     @Override
     public void apply(AST ast) {
         //variableValues = new HANLinkedList<>();
-
+        applyStylesheet((Stylesheet) ast.root);
     }
 
-    
+    private void applyStylesheet(Stylesheet node) {
+        applyStylerule((Stylerule) node.getChildren().get(0));
+    }
+
+    private void applyStylerule(Stylerule node) {
+        for (ASTNode child : node.getChildren()) {
+            if (child instanceof Declaration) {
+                applyDeclaration((Declaration) child);
+            }
+        }
+    }
+
+    private void applyDeclaration(Declaration node) {
+        node.expression = evalExpression(node.expression);
+    }
+
+    private Expression evalExpression(Expression expression) {
+        return null;
+    }
 }
