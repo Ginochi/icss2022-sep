f44ce51f11f3aef38448e1366fed84235dcb9870
Gino Janssen <Ginojanssen23@gmail.com>
2024-10-16 09:24:36 +0200

    checker les code

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -1,7 +1,9 @@
 package nl.han.ica.icss.checker;
 
+import nl.han.ica.datastructures.HANLinkedList;
 import nl.han.ica.datastructures.IHANLinkedList;
-import nl.han.ica.icss.ast.AST;
+import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.literals.PixelLiteral;
 import nl.han.ica.icss.ast.types.ExpressionType;
 
 import java.util.HashMap;
@@ -13,9 +15,30 @@ public class Checker {
     private IHANLinkedList<HashMap<String, ExpressionType>> variableTypes;
 
     public void check(AST ast) {
-        // variableTypes = new HANLinkedList<>();
+        variableTypes = new HANLinkedList<>();
 
+        checkStylesheet(ast.root);
     }
 
-    
+    private void checkStylesheet(Stylesheet node) {
+        checkStylerule((Stylerule) node.getChildren().get(0));
+    }
+
+    private void checkStylerule(Stylerule node) {
+        for (ASTNode child : node.getChildren()) {
+            if (child instanceof Declaration) {
+                checkDeclaration((Declaration) child);
+            }
+        }
+    }
+
+    private void checkDeclaration(Declaration node) {
+        if (node.property.name.equals("width")) {
+            if (!(node.expression instanceof PixelLiteral)) {
+                node.setError("Property 'width' has invalid type");
+            }
+        }
+    }
+
+
 }
