0be5c3d8871d61ca6f7526d9e33c8021a19c4255
Gino Janssen <Ginojanssen23@gmail.com>
2024-10-16 09:21:13 +0200

    Listener Lvl0 finished

--- startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
+++ startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
@@ -4,6 +4,8 @@ package nl.han.ica.icss.parser;
 import nl.han.ica.datastructures.HANStack;
 import nl.han.ica.datastructures.IHANStack;
 import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.selectors.ClassSelector;
+import nl.han.ica.icss.ast.selectors.IdSelector;
 import nl.han.ica.icss.ast.selectors.TagSelector;
 
 /**
@@ -50,7 +52,7 @@ public class ASTListener extends ICSSBaseListener {
 
 	@Override
 	public void enterTagSelector(ICSSParser.TagSelectorContext ctx) {
-		TagSelector tagSelector = new TagSelector("temp TAG");
+		TagSelector tagSelector = new TagSelector(ctx.getText());
 		currentContainer.push(tagSelector);
 	}
 
@@ -58,5 +60,41 @@ public class ASTListener extends ICSSBaseListener {
 	public void exitTagSelector(ICSSParser.TagSelectorContext ctx) {
 		TagSelector selector = (TagSelector) currentContainer.pop();
 		currentContainer.peek().addChild(selector);
+	}
+
+	@Override
+	public void enterIdSelector(ICSSParser.IdSelectorContext ctx) {
+		IdSelector idSelector = new IdSelector(ctx.getText());
+		currentContainer.push(idSelector);
+	}
+
+	@Override
+	public void exitIdSelector(ICSSParser.IdSelectorContext ctx) {
+		IdSelector selector = (IdSelector) currentContainer.pop();
+		currentContainer.peek().addChild(selector);
+	}
+
+	@Override
+	public void enterClassSelector(ICSSParser.ClassSelectorContext ctx) {
+		ClassSelector classSelector = new ClassSelector(ctx.getText());
+		currentContainer.push(classSelector);
+	}
+
+	@Override
+	public void exitClassSelector(ICSSParser.ClassSelectorContext ctx) {
+		ClassSelector selector = (ClassSelector) currentContainer.pop();
+		currentContainer.peek().addChild(selector);
+	}
+
+	@Override
+	public void enterDeclaration(ICSSParser.DeclarationContext ctx) {
+		Declaration declaration = new Declaration(ctx.getText());
+		currentContainer.push(declaration);
+	}
+
+	@Override
+	public void exitDeclaration(ICSSParser.DeclarationContext ctx) {
+		Declaration declaration = (Declaration) currentContainer.pop();
+		currentContainer.peek().addChild(declaration);
 	}
 }
