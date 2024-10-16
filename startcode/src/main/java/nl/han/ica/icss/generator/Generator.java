7e1fa09172c633cfdbc7c24fa96aafbb40ddeaee
Gino Janssen <Ginojanssen23@gmail.com>
2024-10-16 11:33:14 +0200

    generator class demo code

--- startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
+++ startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -2,14 +2,25 @@ package nl.han.ica.icss.generator;
 
 
 import nl.han.ica.icss.ast.AST;
+import nl.han.ica.icss.ast.Stylerule;
+import nl.han.ica.icss.ast.Stylesheet;
 
 public class Generator {
 
 	public String generate(AST ast) {
-        return "";
-
-
+		return generateStylesheet((Stylesheet) ast.root);
 	}
 
-	
+	private String generateStylesheet(Stylesheet root) {
+		return generateStylerule((Stylerule) root.getChildren().get(0));
+	}
+
+	private String generateStylerule(Stylerule stylerule) {
+		String result = stylerule.selectors.get(0) + " {\n";
+		//result += "\t" + generateDeclaration(stylerule.body.get(0));
+		result += "\n}";
+		return result;
+	}
+
+
 }
